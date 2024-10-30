79514325e7256c35b797accbe6757ff61fd072b8
ДимДим <1000rub@list.ru>
2024-10-30 16:00:21 +0300

    rpz301024

--- DZ8Main.c
+++ DZ8Main.c
@@ -6,6 +6,9 @@ int sizeTask2 = 5;  // task2
 void executorTask1(int arg);
 void executorTask2(int size);
 
+void sequenceData(float arr[], int size);
+void printSeqDataConsole(float arr[], int size);
+
 void main()
 {
     printf("Start run DZ8\n\n");
@@ -17,11 +20,12 @@ void main()
 void executorTask1(int arg)
 {
     printf("Task1 run\n");
+    int tempValum[] = {15, -100};
     int *pointer = &arg;
     printf("value Arg = %d, address Arg = %p\n", *pointer, (void *)pointer);
 
     printf("transform Arg...\n");
-    *pointer = ~(*pointer * 15 - 100);
+    *pointer = ~(*pointer * *tempValum + *(tempValum + 1));
 
     printf("newValue Arg = %d, address Arg = %p\n", arg, (void *)pointer);
 
@@ -31,6 +35,7 @@ void executorTask1(int arg)
 void executorTask2(int size)
 {
     printf("Task2 run\n");
+    float tempValum[] = {2, 2.8, 9, -3.3};
     float arr[size];
 
     // наполним массив
@@ -38,19 +43,52 @@ void executorTask2(int size)
     {
         if ((i % 2) == 0)
         {
-            *(arr + i) = i+2/2.8;
+            *(arr + i) = i + *tempValum / *(tempValum + 1);
         }
         else
         {
-            *(arr + i) = i+9 / -3.3;
+            *(arr + i) = i + *(tempValum + 2) / *(tempValum + 3);
         }
     }
     // покажем
     for (int i = 0; i < size; i++)
     {
-        printf("[%f] ", *(arr+i));
+        printf("[%f] ", *(arr + i));
     }
     printf("\n");
+    
+    sequenceData(arr, size);
 
     printf("Stop Task2\n\n");
+}
+
+void sequenceData(float arr[], int size)
+{
+    float temp;
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    printSeqDataConsole(arr, size);
+}
+
+void printSeqDataConsole(float arr[], int size)
+{
+    printf("out seq data -> ");
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] != 0)
+        {
+            printf("%f ", arr[i]);
+        }
+    }
+    printf("\n");
 }
