5f641c9405f184588d164cecb3aa56799d8eeb50
ДимДим <1000rub@list.ru>
2024-10-30 20:33:50 +0300

    dz p3

--- DZ8Main.c
+++ DZ8Main.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
 int argTask1 = 345; // task1
-int sizeTask2 = 5;  // task2
+int sizeTask = 5;  // task2, task3
 
 void executorTask1(int arg);
-void executorTask2(int size);
+void executorTask2(int *size);
+void executorTask3(int *size);
 
-void sequenceData(float arr[], int size);
-void printSeqDataConsole(float arr[], int size);
+float *executorStartArr(float arr[], int *size);// task2, task3
+float *sequenceData(float arr[], int *size);   // task2
+void printDataConsole(float arr[], int *size); // task2, task3
 
 void main()
 {
     printf("Start run DZ8\n\n");
 
     executorTask1(argTask1);
-    executorTask2(sizeTask2);
+    executorTask2(&sizeTask);
+    executorTask3(&sizeTask);
 }
 
 void executorTask1(int arg)
@@ -25,21 +28,50 @@ void executorTask1(int arg)
     printf("value Arg = %d, address Arg = %p\n", *pointer, (void *)pointer);
 
     printf("transform Arg...\n");
-    *pointer = ~(*pointer * *tempValum + *(tempValum + 1));
+    *pointer = ~(*pointer * (*tempValum) + *(tempValum + 1));
 
     printf("newValue Arg = %d, address Arg = %p\n", arg, (void *)pointer);
 
     printf("Stop Task1\n\n");
 }
 
-void executorTask2(int size)
+void executorTask2(int *size)
 {
     printf("Task2 run\n");
+
+    float arr[*size];
+
+    float *arrTemp = executorStartArr(arr, &*size);
+    *arr = *arrTemp;
+    printDataConsole(arr, &*size);
+
+    float *arrSeq = sequenceData(arr, &*size);
+    *arr = *arrSeq;
+    printDataConsole(arr, &*size);
+
+    printf("Stop Task2\n\n");
+}
+
+void executorTask3(int *size)
+{
+    printf("Task3 run\n");
+
+    float arr[*size];
+
+    float *arrTemp = executorStartArr(arr, &*size);
+    *arr = *arrTemp;
+    printDataConsole(arr, &*size);
+
+
+    printf("Stop Task3\n\n");
+}
+
+float *executorStartArr(float arr[], int *size)
+{
     float tempValum[] = {2, 2.8, 9, -3.3};
-    float arr[size];
 
     // наполним массив
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < *size; i++)
     {
         if ((i % 2) == 0)
         {
@@ -50,45 +82,35 @@ void executorTask2(int size)
             *(arr + i) = i + *(tempValum + 2) / *(tempValum + 3);
         }
     }
-    // покажем
-    for (int i = 0; i < size; i++)
-    {
-        printf("[%f] ", *(arr + i));
-    }
-    printf("\n");
-    
-    sequenceData(arr, size);
-
-    printf("Stop Task2\n\n");
+    return arr;
 }
 
-void sequenceData(float arr[], int size)
+float *sequenceData(float arr[], int *size)
 {
     float temp;
-    for (int i = 0; i < size - 1; i++)
+    float *poinTemp = &temp;
+
+    for (int i = 0; i < *size - 1; i++)
     {
-        for (int j = 0; j < size - i - 1; j++)
+        for (int j = 0; j < *size - i - 1; j++)
         {
-            if (arr[j] > arr[j + 1])
+            if (*(arr + j) > *(arr + j + 1))
             {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                *poinTemp = *(arr + j);
+                *(arr + j) = *(arr + j + 1);
+                *(arr + j + 1) = *poinTemp;
             }
         }
     }
-    printSeqDataConsole(arr, size);
+    return arr;
 }
 
-void printSeqDataConsole(float arr[], int size)
+void printDataConsole(float arr[], int *size)
 {
-    printf("out seq data -> ");
-    for (int i = 0; i < size; i++)
+    printf("out data -> ");
+    for (int i = 0; i < *size; i++)
     {
-        if (arr[i] != 0)
-        {
-            printf("%f ", arr[i]);
-        }
+        printf("[%f] ", *(arr + i));
     }
     printf("\n");
 }
