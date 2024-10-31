f33ed71a00f8f3eb1db69485dc688f9406338c21
ДимДим <1000rub@list.ru>
2024-10-31 20:33:00 +0300

    DZ8

--- DZ8Main.c
+++ DZ8Main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int argTask1 = 345; // task1
-int sizeTask = 5;  // task2, task3
+int sizeTask = 5;   // task2, task3
 
 void executorTask1(int arg);
 void executorTask2(int *size);
 void executorTask3(int *size);
 
-float *executorStartArr(float arr[], int *size);// task2, task3
-float *sequenceData(float arr[], int *size);   // task2
-void printDataConsole(float arr[], int *size); // task2, task3
+float *executorStartArr(float arr[], int *size);              // task2, task3
+float *sequenceData(float arr[], int *size);                  // task2, task3
+void printDataConsole(float arr[], int *size);                // task2, task3
+float deltaArr(float arr[], int *size);                       // task3
+float medianArr(float arr[], int *size);                      // task3
+float *findMinMaxArr(float outArr[], float arr[], int *size); // task3
 
 void main()
 {
@@ -18,6 +21,7 @@ void main()
     executorTask1(argTask1);
     executorTask2(&sizeTask);
     executorTask3(&sizeTask);
+    printf("\nEnd DZ8\n\n");
 }
 
 void executorTask1(int arg)
@@ -62,8 +66,64 @@ void executorTask3(int *size)
     *arr = *arrTemp;
     printDataConsole(arr, &*size);
 
+    float deltaTemp;
+    float *delta = &deltaTemp;
+    *delta = deltaArr(arr, &*size);
+    printf("\ndelta = %.4f\n", *delta);
+
+    float medianTemp;
+    float *median = &medianTemp;
+    *median = medianArr(arr, &*size);
+    printf("\nmedian = %.4f\n", *median);
+
+    float valumFind[2] = {};// 0-min 1-max
+    float *valumFindTemp = findMinMaxArr(valumFind, arr, &*size);
+    *valumFind = *valumFindTemp;
+    printf("\nvalumMin = %.4f\n", *valumFind);// 0-min
+    printf("\nvalumMax = %.4f\n", *(valumFind + 1));// 1-max
 
     printf("Stop Task3\n\n");
+}
+
+float deltaArr(float arr[], int *size)
+{
+    float delta;
+    float *poinDelta = &delta;
+
+    for (int i = 0; i < *size; i++)
+    {
+        *poinDelta += *(arr + i);
+    }
+
+    *poinDelta = *poinDelta / *size;
+    return *poinDelta;
+}
+
+float medianArr(float arr[], int *size)
+{
+    float median;
+    float *poinMedian = &median;
+    float *arrSeq = sequenceData(arr, &*size);
+    *arr = *arrSeq;
+
+    if (*size % 2 == 0)
+    {
+        *poinMedian = (*(arr + (*size / 2)) + *(arr + (*size / 2 + 1))) / 2; // при четном (серединка[x]+серединка[x+1])/2=сред.серединок
+    }
+    else
+    {
+        *poinMedian = *(arr + (*size / 2 + 1)); // или при нечетном середина массива
+    }
+    return *poinMedian;
+}
+
+float *findMinMaxArr(float outArr[], float arr[], int *size)
+{
+    float *arrSeq = sequenceData(arr, &*size);
+    
+    *outArr = *arrSeq;// 0-min
+    *(outArr + 1) = *(arrSeq + *size - 1);// 1-max
+    return outArr;
 }
 
 float *executorStartArr(float arr[], int *size)
